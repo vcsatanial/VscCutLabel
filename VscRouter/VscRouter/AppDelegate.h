7522a8e2adb33ca5df1f33e353017e3ff6a12a09
Vincent <116359398@qq.com>
2017-12-21 22:49:54 +0800

    Fix bug

---
+++ VscRouter/VscRouter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VscRouter
+//
+//  Created by tianyan on 2017/12/21.
+//  Copyright © 2017年 Vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VscRouter/VscRouter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VscRouter
+//
+//  Created by tianyan on 2017/12/21.
+//  Copyright © 2017年 Vincent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
